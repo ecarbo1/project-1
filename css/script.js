8c5f654c4718801280206cb6efe0e38e9b3e6398
ecarbo1 <78318096+ecarbo1@users.noreply.github.com>
2021-10-19 20:50:19 -0500

    Update script.js

---
+++ css/script.js
@@ -0,0 +1,42 @@
+var h = document.getElementsByTagName('html')[0];
+var b = document.getElementById('submit');
+
+
+function jschg(){
+  h.setAttribute('class', 'hasjs');   
+}
+
+function btnClick(){	
+  b.style.backgroundColor = 'grey';
+  b.style.color = 'white';
+	
+  var name = document.getElementsByName('fname_field')[0].value + ' ' + document.getElementsByName('lname_field')[0].value;
+  alert('The account ' + name + ' has been made');
+}
+
+function radiofocus(i){
+  document.getElementsByTagName('label')[i+4].style.color = 'orange';
+}
+
+function radioblur(i){
+  document.getElementsByTagName('label')[i+4].style.color = 'black';
+}
+
+window.addEventListener('load', jschg);
+b.addEventListener('click', btnClick);
+
+document.getElementsByName('news_field')[0].addEventListener('focus', function(){radiofocus(0);});
+document.getElementsByName('news_field')[1].addEventListener('focus', function(){radiofocus(1);});
+document.getElementsByName('news_field')[2].addEventListener('focus', function(){radiofocus(2);});
+document.getElementsByName('news_field')[3].addEventListener('focus', function(){radiofocus(3);});
+
+document.getElementsByName('news_field')[0].addEventListener('blur', function(){radioblur(0);});
+document.getElementsByName('news_field')[1].addEventListener('blur', function(){radioblur(1);});
+document.getElementsByName('news_field')[2].addEventListener('blur', function(){radioblur(2);});
+document.getElementsByName('news_field')[3].addEventListener('blur', function(){radioblur(3);});
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
